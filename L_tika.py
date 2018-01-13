12ea4009dcc0d06eaf54641f9516bc6f1b9a4f27
Ishikawa_N <naokingu0801@gmail.com>
2018-01-13 22:57:33 +0900

    Update L_tika.py

--- L_tika.py
+++ L_tika.py
@@ -8,21 +8,21 @@ import subprocess
 #main
 if __name__ == '__main__':
     ### init io port ###
-    subprocess.check_call('gpio export 11 out',shell=True)
-    subprocess.check_call('gpio export 8 out',shell=True)
+    subprocess.check_call('gpio export 25 out',shell=True)
+    #subprocess.check_call('gpio export 8 out',shell=True)
     ###
     rospy.init_node('ledflash')
 
     io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
-    io.pinMode(11,io.OUTPUT)  # Setup pin 11
-    io.pinMode(8,io.OUTPUT)  # Setup pin 8 
+    io.pinMode(25,io.OUTPUT)  # Setup pin 11
+    #io.pinMode(8,io.OUTPUT)  # Setup pin 8 
 
     while not rospy.is_shutdown():
-        io.digitalWrite(11,1)
-        io.digitalWrite(8,0)
+        io.digitalWrite(25,1)
+        #io.digitalWrite(8,0)
         time.sleep(1)
-        io.digitalWrite(11,0)
-        io.digitalWrite(8,1)
+        io.digitalWrite(25,0)
+        #io.digitalWrite(8,1)
         time.sleep(1)
 
     print('\rStopped')
